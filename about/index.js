9df40068dafdd3c043e079fbee37b57397bc3368
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2019-03-05 21:40:00 +0800

    replace route

--- about/index.js
+++ about/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("*", (req, res) => {
   console.log("req", req);
