29df767e934508b6c53897fe5f3f2214cd0b2b4e
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2019-03-06 14:18:49 +0800

    replace route

--- about/index.ts
+++ about/index.ts
@@ -1,24 +1,45 @@
 import * as express from "express";
 var app = express();
 
+import {
+  Context,
+  ServiceContext,
+  Server,
+  Path,
+  GET,
+  PathParam,
+  POST
+} from "typescript-rest";
+
 import * as multer from "multer";
 var upload = multer();
 
-app.get("*", (req, res) => {
-  console.log("req", req);
-  console.log("res", res);
-  res.write(
-    "<h1><marquee direction=left>Hello from Express path `/about` on Now 2.0!</marquee></h1>"
-  );
-  res.write('<h2>Go to <a href="/">/</a></h2>');
-  res.end();
-});
+@Path("/")
+class AboutService {
+  @GET
+  getAbout(
+    @Context
+    context: ServiceContext
+  ): void {
+    console.log("req", context.request);
+    console.log("res", context.response);
+    context.response.write(
+      "<h1><marquee direction=left>Hello from Express path `/about` on Now 2.0!</marquee></h1>"
+    );
+    context.response.write('<h2>Go to <a href="/">/</a></h2>');
+    context.response.end();
+  }
 
-app.post("*", upload.array(), function(req, res) {
-  console.log("req", req);
-  console.log("req.body", req.body);
-  console.log("res", res);
-  res.send("Hello API Event Received");
-});
+  @POST
+  postAbout(
+    @Context
+    context: ServiceContext
+  ) {
+    console.log("req", context.request);
+    console.log("req.body", context.request.body);
+    console.log("res", context.response);
+    context.response.send("Hello API Event Received");
+  }
+}
 
 module.exports = app;
