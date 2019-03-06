9f37d055cb6427cba8cb0091aa1e7a9440f2407b
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2019-03-06 12:20:18 +0800

    replace route

--- about/index.js
+++ about/index.js
@@ -1,25 +1,20 @@
-const express = require("express");
-const app = express();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
 var multer = require("multer");
 var upload = multer();
-
-app.get("*", (req, res) => {
-  console.log("req", req);
-  console.log("res", res);
-  res.write(
-    "<h1><marquee direction=left>Hello from Express path `/about` on Now 2.0!</marquee></h1>"
-  );
-  res.write('<h2>Go to <a href="/">/</a></h2>');
-  res.write("req", req);
-  res.end();
+app.get("*", function (req, res) {
+    console.log("req", req);
+    console.log("res", res);
+    res.write("<h1><marquee direction=left>Hello from Express path `/about` on Now 2.0!</marquee></h1>");
+    res.write('<h2>Go to <a href="/">/</a></h2>');
+    res.end();
 });
-
-app.post("*", upload.array(), function(req, res) {
-  console.log("req", req);
-  console.log("req.body", req.body);
-  console.log("res", res);
-  res.send("Hello API Event Received");
+app.post("*", upload.array(), function (req, res) {
+    console.log("req", req);
+    console.log("req.body", req.body);
+    console.log("res", res);
+    res.send("Hello API Event Received");
 });
-
 module.exports = app;
---
+++ about/index.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+var app = express();
+
+import * as multer from "multer";
+var upload = multer();
+
+app.get("*", (req, res) => {
+  console.log("req", req);
+  console.log("res", res);
+  res.write(
+    "<h1><marquee direction=left>Hello from Express path `/about` on Now 2.0!</marquee></h1>"
+  );
+  res.write('<h2>Go to <a href="/">/</a></h2>');
+  res.end();
+});
+
+app.post("*", upload.array(), function(req, res) {
+  console.log("req", req);
+  console.log("req.body", req.body);
+  console.log("res", res);
+  res.send("Hello API Event Received");
+});
+
+module.exports = app;
