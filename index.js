82aee17569973cf6694f4df7690b9a5c97616dd2
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2019-03-07 11:13:50 +0800

    replace route

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var AboutService = /** @class */ (function () {
         console.log("GREGGGGG");
         console.log("req", context.request);
         console.log("req.body", context.request.body);
-        console.log("file", file);
+        // console.log("file", file);
         console.log("res", context.response);
         context.response.send("Hello API Event Received");
     };
